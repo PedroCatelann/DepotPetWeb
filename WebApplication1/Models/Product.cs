497c909a8d72c917551871ceb7133d402d77a577
PedroCatelann <pedrohfktlan@gmail.com>
2023-09-11 15:12:55 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        
+    }
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,33 @@
+
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews(x => x.SuppressAsyncSuffixInActionNames = false).AddRazorRuntimeCompilation();
+
+builder.Services.AddDbContext<AppDbContext>(op => op.UseSqlServer("Data Source=localhost;Initial Catalog=depotpetweb_db;Integrated Security=True"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Product}/{action=Index}/{id?}");
+
+app.Run();
